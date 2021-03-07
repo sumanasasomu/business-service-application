61f323e9f64ec3e6754d0c93241517974caf2b84
LalithSrinivas <srinivaskaladi3096@gmail.com>
2021-03-07 21:59:34 +0530

    customer page - backend

--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,85 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, session
 from flask_cors import CORS
+import pymysql
 import json
+import datetime as dt
+
+conn = pymysql.connect( 
+        host='localhost', 
+        user='root',  
+        password = "password", 
+        db='office_database', 
+        ) 
+cur = conn.cursor()
 
 app = Flask(__name__)
-# CORS(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+customer_session_details = None
+app.secret_key = "secretKey"
 
-
-@app.route('/api/hello')
+@app.route('/api/home')
 def say_hello_world():
-    return {'result': "Hello World"}
+	try:
+		cur.execute("select * from stock")
+		output = cur.fetchall()
+		print(output)
+		return {'result': str(output)}
+	except :
+		print("Caused an exception!")
+	return {'result':"sorry!"}
 
-@app.route('/api/hola', methods=['GET', 'POST'])
-def say_hola_world():
-    if request.method == 'POST':
-        formState = json.loads(request.data)
-        print('The form value for alignment is', formState['alignment'])
-        return {'result': "Hola World"}
+@app.route('/api/cutomerrequest', methods=['GET', 'POST'])
+def customerRequest():
+	global customer_session_details
+	if request.method == 'POST':
+		# print(request.form)
+		data = request.form
+		customer_session_details = data
+		if customer_session_details.get('date') is None:
+			customer_session_details['date'] = dt.datetime.now()
+		name = data.get('customerName')
+		phone = data.get('customerPhone')
+		mail = data.get('customerEmail')
+		try:
+			cur.execute("""
+							insert into
+							customers (name, phone_number, email_id)
+							values (\'{}\', \'{}\', \'{}\');
+						""".format(name, phone, mail))
+			conn.commit()
+			cur.execute("""
+							select customer_id from customers
+							where name = \'{}\'
+							and phone_number = \'{}\'
+							and email_id = \'{}\';
+						""".format(name, phone, mail))
+			result = cur.fetchall()
+			customer_id = result[0][0]
+			session['customer_id'] = customer_id
+			return registerVehicle()
+		except Exception as e:
+			print(e)
+			return {'result': "Bad response"}
 
-# FLASK_APP=server.py flask run
+def registerVehicle():
+	if customer_session_details.get('vehicleNumber') is not None:
+		try:
+			cur.execute("""
+							insert ignore into vehicles
+							set vehicle_id = \'{}\',
+							car_model_name = \'{}\',
+							customer_id = {},
+							last_visit = \'{}\';
+						""".format(customer_session_details.get('vehicleNumber'), \
+								customer_session_details.get('carModel'), \
+									customer_session_details.get('date'), \
+										session.get('customer_id')))
+			conn.commit()
+		except Exception as e:
+			print(e)
+			return {'result': "Bad response at vehicle"}
+		return {'result': 'success'}
+	return {'result': 'fail', 'message': 'no vehicle number'}
 
-
+if __name__ == "__main__":
+	app.run(host="localhost", port=5002, debug=True)
