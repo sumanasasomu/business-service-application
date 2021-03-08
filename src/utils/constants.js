8f16036127d7b0d9e4cee67eeb9c77286786c402
sumanasasomu <sumanasasomu@gmail.com>
2021-03-08 20:12:29 +0530

    v1.4 validation added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,10 +5,12 @@ export const themeColors = {
     white: "#ffffff",
     primary: "#5a2c84",
     secondary: "#a78abe",
-    tertiary: "#f2ebf7"
+    tertiary: "#f2ebf7",
+    darkPrimary: '#21093d'
 }
 export const statePrototype = {
     form1Responses: {},
     form2Responses: {},
     shouldAskModel: true
 }
+export const backgroundImage = "https://www.budgetdirect.com.au/blog/wp-content/uploads/2019/10/Tyre-Pressures-In-Cars.jpg"
