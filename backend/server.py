9225bda9b561a589b55ea762994ede503ac0ebc3
LalithSrinivas <srinivaskaladi3096@gmail.com>
2021-03-08 20:05:08 +0530

    payment page - backend update

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session
+from flask import Flask, request, session, jsonify
 from flask_cors import CORS
 import pymysql
 import json
@@ -27,7 +27,15 @@ def say_hello_world():
 		print("Caused an exception!")
 	return {'result':"sorry!"}
 
-@app.route('/api/cutomerrequest', methods=['GET', 'POST'])
+@app.route('/api/brands')
+def getBrands():
+    cur.execute("select brand_id, brand_name from tyre_brand")
+    result = cur.fetchall()
+    list_ = [{'value': i[0], 'label': i[1][0].upper()+i[1][1:]} for i in result]
+    # print(list_)
+    return jsonify(list_)
+
+@app.route('/api/customer', methods=['GET', 'POST'])
 def customerRequest():
 	global customer_session_details
 	if request.method == 'POST':
@@ -70,7 +78,7 @@ def registerVehicle():
 							customer_id = {},
 							last_visit = \'{}\';
 						""".format(customer_session_details.get('vehicleNumber'), \
-								customer_session_details.get('carModel'), \
+								customer_session_details.get('vehicleModel'), \
 									customer_session_details.get('date'), \
 										session.get('customer_id')))
 			conn.commit()
@@ -82,61 +90,84 @@ def registerVehicle():
 
 @app.route('/api/paymentPage', methods=['GET', 'POST'])
 def registerPayment():
-	if request.method == 'POST':
-		try:
-			data = request.form
-			vehicle_id = customer_session_details.get('vehicleNumber')
-			customer_id = session.get('customer_id')
-			services = customer_session_details.get('services')
-			isTyreSale = True if services%2 != 0 else False
-			payment_type = data.get('payment_type')
-			isPaymentDone = data.get('isPaymentDone')
-			paymentId = data.get('paymentId')
-			date = customer_session_details.get('date')
-			totalAmount = data.get('totalAmount')
-			cur.execute("""
-							insert into transactions 
-								(transaction_date, 
-								customer_id, vehicle_id, 
-								service_type, is_tyre_sale, 
-								amount, payment_type, 
-								is_payment_done, payment_id)
-							values (\'{}\', {}, \'{}\', {}, {}, {}, {}, {}, \'{}\');
-						""".format(date, customer_id, vehicle_id, services, isTyreSale, \
-							totalAmount, payment_type, isPaymentDone, paymentId))
-			conn.commit()
-			cur.execute("select LAST_INSERT_ID();")
-			transaction_id = cur.fetchall()[0][0]
-			if isTyreSale:
-				tyreSize = data.get('tyreSize')
-				quantity = data.get('numTyres')
-				eachTyrePrice = data.get('tyrePrice')
-				amount = quantity*eachTyrePrice
-				tyreBrand = data.get('tyreBrand')
-				cur.execute("select tyre_id from stock, tyre_brand where tyre_size=\'{}\' \
+    if request.method == 'POST':
+        try:
+            data = request.form
+            vehicle_id = customer_session_details.get('vehicleNumber')
+            customer_id = session.get('customer_id')
+            services = customer_session_details.get('services')
+            isTyreSale = True if services%2 != 0 else False
+            payment_type = data.get('payment_type')
+            isPaymentDone = data.get('isPaymentDone')
+            paymentId = data.get('paymentId')
+            date = customer_session_details.get('date')
+            totalAmount = data.get('totalAmount')
+            cur.execute("""
+                            insert into transactions 
+                                (transaction_date, 
+                                customer_id, vehicle_id, 
+                                service_type, is_tyre_sale, 
+                                amount, payment_type, 
+                                is_payment_done, payment_id)
+                            values (\'{}\', {}, \'{}\', {}, {}, {}, {}, {}, \'{}\');
+                        """.format(date, customer_id, vehicle_id, services, isTyreSale, \
+                            totalAmount, payment_type, isPaymentDone, paymentId))
+            conn.commit()
+            cur.execute("select LAST_INSERT_ID();")
+            transaction_id = cur.fetchall()[0][0]
+            if isTyreSale:
+                tyreSize = data.get('tyreSize')
+                quantity = data.get('numTyres')
+                eachTyrePrice = data.get('tyrePrice')
+                amount = quantity*eachTyrePrice
+                tyreBrand = data.get('tyreBrand')
+                cur.execute("""
+                                replace into stock (tyre_size, brand_id, quantity)
+                                select \'{}\' as tyre_size, tyre_brand.brand_id, ifnull(max(quantity), 0)
+                                from stock, tyre_brand
+                                where tyre_brand.brand_id = stock.brand_id
+                                and tyre_size = \'{}\'
+                                and tyre_brand.brand_name = \'{}\'
+                            """.format(tyreSize, tyreSize, tyreBrand))
+                conn.commit()
+                cur.execute("select tyre_id from stock, tyre_brand where tyre_size=\'{}\' \
 								and stock.brand_id = tyre_brand.brand_id \
 									and brand_name = \'{}\'".format(tyreSize, tyreBrand))
-				tyre_id = cur.fetchall()[0][0]
-				cur.execute("""
-								insert into tyres_transactions (transaction_id, tyre_id, num_tyres, amount)
-								values ({}, \'{}\', {}, {})
-							""".format(transaction_id, tyre_id, quantity, amount))
-			
-			return {'result': 'success'}
-		except:
-			return {'result': 'failed'}
+                tyre_id = cur.fetchall()[0][0]
+                cur.execute("""
+                                insert into tyres_transactions (transaction_id, tyre_id, num_tyres, amount)
+                                values ({}, \'{}\', {}, {})
+                            """.format(transaction_id, tyre_id, quantity, amount))
+                return {'result': 'success'}
+        except:
+            pass
+        return {'result': 'failed'}
 
 @app.route('/api/test', methods=['POST'])
 def autoIncrementTest():
-	data = request.form
-	print(data)
-	brand_name = data.get('brandName')
-	cur.execute("insert into tyre_brand (brand_name) values (\'{}\');".format(brand_name))
-	conn.commit()
-	cur.execute("select LAST_INSERT_ID();")
-	res = cur.fetchall()
-	print(res[0][0])
-	return {'result':str(res)}
+    data = request.form
+    tyreSize = data.get('tyreSize')
+    tyreBrand = data.get('tyreBrand')
+    cur.execute("select brand_id from tyre_brand where tyre_brand.brand_name = \'{}\'".format(tyreBrand))
+    res = cur.fetchall()
+    print(res)
+    cur.execute("""
+                    insert ignore into stock (tyre_size, brand_id, quantity)
+                    select \'{}\' as tyre_size, {}, ifnull(max(quantity), 0)
+                    from stock
+                    where stock.brand_id = {}
+                    and tyre_size = \'{}\'
+                """.format(tyreSize, res[0][0], res[0][0], tyreSize))
+    conn.commit()
+    cur.execute("""
+                    select *
+                    from stock, tyre_brand
+                    where tyre_brand.brand_id = stock.brand_id
+                    and tyre_size = \'{}\'
+                    and tyre_brand.brand_name = \'{}\' 
+                """.format(tyreSize, tyreBrand))
+    res = cur.fetchall()
+    return {'result': str(res)}
 
 if __name__ == "__main__":
 	app.run(host="localhost", port=5002, debug=True)
